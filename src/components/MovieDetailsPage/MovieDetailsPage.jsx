959abc126dcdea198360ee3c9b3d6401e571d472
Katrin <kateyka1410@gmail.com>
2024-08-18 22:10:04 +0300

    Update MovieDetailsPage.jsx

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,9 +1,35 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import css from "./MovieDetailsPage.module.css";
 
+import { useEffect, useState } from "react";
+import { fetchMovieDetails } from "../../fetchApi";
+
 const MovieDetails = () => {
+  const { movieId } = useParams(); 
+  const [movieDetails, setMovieDetails] = useState(null); 
+
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        const data = await fetchMovieDetails(movieId);
+        setMovieDetails(data); 
+      } catch (error) {
+         toast.error("Something went wrong. You can try again later");
+      }
+    };
+
+    getMovieDetails();
+  }, [movieId]); 
+
   return (
     <div className={css.wrapper}>
+      {movieDetails && ( 
+        <div>
+          <h2>{movieDetails.title}</h2>
+          <p>{movieDetails.overview}</p>
+        </div>
+      )}
+
       <h3>Additional information</h3>
       <ul>
         <li>
